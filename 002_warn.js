153a2e411a3f2dcddf330dd0a22eb69becbd148a
TAEKnical <ilikebigamc@naver.com>
2019-11-01 12:13:53 +0900

    day 1

---
+++ 002_warn.js
@@ -0,0 +1,7 @@
+console.log('name:%s', 'jintae');
+console.log('age:%d', 31);
+console.log('pi:%d', 3.14);
+console.log('math:%d science:%d', 92, 84);
+console.log('name:%s age:%d', 'jintae', 31);
+console.log('name:%s math:%d science:%d', 'jintae', 92, 84);
+console.log('name:%s math:%s science:%s', 'jintae', 92, 84);
---
+++ 006_backtic.js
@@ -0,0 +1,12 @@
+const greeting1 = 'hello';
+const greeting2 = 'byte';
+const name1 = 'jintae';
+const name2 = 'other';
+
+const statement = `${greeting1}! my name is ${name2}`;
+const statement2 = `${greeting2}! my name is ${name2}`;
+
+console.log(`${greeting1}! my name is ${name2}`);
+console.log(`${greeting2}! my name is ${name2}`);
+console.log('statement : ', statement);
+console.log('statement2 : ', statement2);
